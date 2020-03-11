bcdf988c871fe4b25907101265579596c5c91bbe
N1Anoobis <mrbond84@hotmail.com>
2020-03-11 11:58:18 +0100

    Add listiner to clicked element also search for article by taken id, and add class active to it

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 //   });
 
 const titleClickHandler = function(event){
+  event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log('clickedElement (with plus): ' + clickedElement);
@@ -14,7 +15,7 @@ const titleClickHandler = function(event){
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
-    /* [IN PROGRESS]add class 'active' to the clicked link */
+    /* [DONE]add class 'active' to the clicked link */
     clickedElement.classList.add('active');
      /* [DONE]remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts .post');
@@ -23,10 +24,13 @@ const titleClickHandler = function(event){
       activeArticle.classList.remove('active');
     }
     /* get 'href' attribute from the clicked link */
-  
+    let attribute = clickedElement.getAttribute('href')
+    console.log(attribute)
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+    targetArticle = document.querySelector(attribute)
+    console.log(targetArticle)
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
